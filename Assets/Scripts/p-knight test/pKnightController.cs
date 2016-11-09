5e9a5d8d9ac3a2c9b3657ea080782875e7ebfc14
Mariano <mmontori@umich.edu>
2016-11-09 14:57:22 -0500

    added some testing
    worked with using the import sprite and other stuff using the camera

---
+++ Assets/Scripts/p-knight test/pKnightController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pKnightController : MonoBehaviour {
+
+	private GameObject pKnight;
+	private Rigidbody2D rb;
+	public int walkSpeed;
+	public int jumpHeight;
+
+
+	// Use this for initialization
+	void Start () {
+		pKnight = this.gameObject;
+		rb = pKnight.GetComponent<Rigidbody2D>();
+		rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+		walkSpeed = 50;
+		jumpHeight = 150;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement ();
+	}
+
+	void movement(){
+		if(Input.GetKeyDown("up")){
+			rb.velocity = new Vector2 (rb.velocity.x, jumpHeight);
+		}
+		if(Input.GetKey("right")){
+			rb.velocity = new Vector2 (walkSpeed, rb.velocity.y);
+		}
+		if(Input.GetKey("left")){
+			rb.velocity = new Vector2 (-walkSpeed, rb.velocity.y);
+		}
+	}
+}
