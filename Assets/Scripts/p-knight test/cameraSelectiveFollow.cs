fb9b6a1c8312d2d95eb4452ead3eb3a9744451c4
Mariano <mmontori@umich.edu>
2016-11-16 22:03:14 -0500

    transition up, starting transition down
    to work on next:
    -finish transition up/down
    -start transition left/right
    -clamping location
    -clean up
    
    (create an array/list of focus areas(or new struct)? to decide where to
    transition?)

--- Assets/Scripts/p-knight test/cameraSelectiveFollow.cs
+++ Assets/Scripts/p-knight test/cameraSelectiveFollow.cs
@@ -3,24 +3,24 @@ using System.Collections;
 
 public class cameraSelectiveFollow : MonoBehaviour {
 
-//	private bool once;
-//	private float b;
-//	private Vector3 ab;
+	private bool once;
+	private float b;
+	private Vector3 ab;
 	public float cameraDistance;
 	public float airHeight;
-//	public float cameraTop;
-//	public float cameraBottom;
+	public float cameraTop;
+	public float cameraBottom;
 	public pKnightController target;
 	public Vector2 focusAreaSize;
 	FocusArea focusArea;
 
 	void Awake () {
-//		once = false;
+		once = false;
 		cameraDistance = 90f;
 		GetComponent<UnityEngine.Camera>().orthographicSize = cameraDistance;
-//		cameraTop = transform.position.y + cameraDistance - 23f;
-//		cameraBottom = transform.position.y - cameraDistance;
-//		Debug.Log (cameraTop);
+		cameraTop = transform.position.y + cameraDistance - 23f;
+		cameraBottom = transform.position.y - cameraDistance;
+		Debug.Log (cameraTop);
 	}
 
 	void Start () {
@@ -30,27 +30,38 @@ public class cameraSelectiveFollow : MonoBehaviour {
 
 	void LateUpdate () {
 		focusArea.Update (target.spriteRender.bounds);
-		transform.position = new Vector3 (focusArea.center.x, focusArea.center.y + airHeight, transform.position.z);
-//		if (focusArea.center.y < cameraTop) {
-//			transform.position = new Vector3 (focusArea.center.x, transform.position.y, transform.position.z);
-//		}
+//		transform.position = new Vector3 (focusArea.center.x, focusArea.center.y + airHeight, transform.position.z);
+		if (focusArea.center.y < cameraTop) {
+			transform.position = new Vector3 (focusArea.center.x, transform.position.y, transform.position.z);
+		}
+		else if(focusArea.center.y < cameraBottom){
+			cameraTop -= (2*cameraDistance);
+			cameraBottom -= (2 * cameraDistance);
+			Debug.Log (2 * cameraDistance);
+			Debug.Log (transform.position.y);
+			b = transform.position.y - 2*cameraDistance + airHeight;
+			Debug.Log (b);
+			ab = new Vector3 (transform.position.x, b, transform.position.z);
+			once = true;
+		}
 //		else{
 //			cameraTop += (2*cameraDistance);
 //			cameraBottom += (2 * cameraDistance);
 //			Debug.Log (2 * cameraDistance);
 //			Debug.Log (transform.position.y);
-//			b = transform.position.y + 2*cameraDistance;
+//			b = transform.position.y + 2*cameraDistance - airHeight;
+//			Debug.Log (b);
 //			ab = new Vector3 (transform.position.x, b, transform.position.z);
 //			once = true;
 ////			float a = transform.position.y;
 ////			Debug.Log (b);
 ////			transform.position = Vector3.Lerp (transform.position, ab, 0.01f);
 ////			Mathf.Lerp (a, b, 0.5f);
-//			Debug.Log (cameraTop);
+//			Debug.Log (cameraBottom);
 //		}
-//		if (once) {
-//			StartCoroutine (transition (1000f));
-//		}
+		if (once) {
+			StartCoroutine (transition (1000f));
+		}
 //		Debug.Log (target.spriteRender.bounds);
 
 //		if(focusArea.center.y > 60){
@@ -109,13 +120,13 @@ public class cameraSelectiveFollow : MonoBehaviour {
 		}
 	}
 
-//	private IEnumerator transition(float time){
-//		float i = 0.0f;
-//		float rate = 1.0f/time;
-//		while (i < 1.0f) {
-//			i += Time.deltaTime * rate;
-//			transform.position = Vector3.Lerp (transform.position, ab, i);
-//			yield return new WaitForSeconds(i);
-//		}
-//	}
+	private IEnumerator transition(float time){
+		float i = 0.0f;
+		float rate = 1.0f/time;
+		while (i < 1.0f) {
+			i += Time.deltaTime * rate;
+			transform.position = Vector3.Lerp (transform.position, ab, i);
+			yield return new WaitForSeconds(i);
+		}
+	}
 }
